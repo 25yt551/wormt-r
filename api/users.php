650127fe482ca84ba822f7ca1e3a473b394fec0a
25yt551 <amirrchr@gmail.com>
2025-08-12 19:37:53 +0200

    Update users.php

--- api/users.php
+++ api/users.php
@@ -104,6 +104,25 @@
               "Client_VisibleSkin8": "0000",
               "Client_VisibleSkin9": "0000",
               "Client_VisibleSkin10": "0000"
+        },
+        {
+              "id": 7,
+              "cliente_NOMBRE": "hallow",
+              "cliente_ID": "gg_101404793016664189589",
+              "Client_KeyAccecs": "XTPRIVATESKIN",
+              "cliente_DateExpired": "2025-10-01",
+              "status": 1,
+              "Client_VisibleSkin": "0000",
+              "Client_VisibleSkin1": "0000",
+              "Client_VisibleSkin2": "0000",
+              "Client_VisibleSkin3": "0000",
+              "Client_VisibleSkin4": "0000",
+              "Client_VisibleSkin5": "0000",
+              "Client_VisibleSkin6": "0000",
+              "Client_VisibleSkin7": "0000",
+              "Client_VisibleSkin8": "0000",
+              "Client_VisibleSkin9": "0000",
+              "Client_VisibleSkin10": "0000"
         }
     ],
     "timestamp": 1754244868,
